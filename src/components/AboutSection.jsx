cb39d52e69b297e8b6fef0bbd51e4d4ad66563d7
SellerOfDigitalBalance <faiz.muhammad.rijal.fikri@gmail.com>
2026-01-24 12:46:48 +0700

    Modify/Download_CV

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -32,7 +32,7 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href=""
+                href="https://drive.google.com/file/d/12QucYethznqm5qWUmkF6YmzcJrg9gkWK/view?usp=drive_link"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
